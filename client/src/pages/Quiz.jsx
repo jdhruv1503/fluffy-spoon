64cf91ac697c77733b72282f8de23fb5bb9cf53e
jdhruv1503 <jdhruv1503@gmail.com>
2024-02-01 03:44:06 +0530

    Added fill-in-the-blank questions

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import FillInTheBlanks from "../components/Questions/FillInTheBlanks";
 
 export default function Quiz() {
   return (
     <>
       <Sidebar qNo={10} />
-      <div className="pt-28">
-        <div className="px-4 py-12 max-w-4xl mx-auto">
-          <h1 className="text-3xl font-bold  mb-4 text-slate-800">
-            Welcome to Quiz!
-          </h1>
-          <p className="mb-4 text-slate-700">Ths is a quiz.</p>
-          <p className="mb-4 text-slate-700">
-            The front-end of the application is built with React and uses React
-            Router for client-side routing. The back-end is built with Node.js
-            and Express, and uses MongoDB as the database. Authentication is
-            implemented using JSON Web Tokens (JWT).
-          </p>
-          <p className="mb-4 text-slate-700">
-            This application is intended as a starting point for building
-            full-stack web applications with authentication using the MERN
-            stack. Feel free to use it as a template for your own projects!
-          </p>
-        </div>
-      </div>
+      <FillInTheBlanks
+        queNo={1}
+        questionDetails={{
+          questions: ["1+1 = ", "1+1 = ", "1+1 = ", "1+1 = "],
+        }}
+      />
     </>
   );
 }
