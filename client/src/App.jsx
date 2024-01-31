7085019cdd1281f99d55fb8624af4014a2f0b0af
jdhruv1503 <jdhruv1503@gmail.com>
2024-02-01 00:10:04 +0530

    housekeeping, 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Quiz from './pages/Quiz';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
@@ -14,12 +15,20 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
         </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/quiz' element={<Quiz />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
