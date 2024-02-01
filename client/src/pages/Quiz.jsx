0486da6fb7c10cbe003857da7b8d9d1e01c42069
jdhruv1503 <jdhruv1503@gmail.com>
2024-02-01 17:05:17 +0530

    added quiz page

--- client/src/pages/Quiz.jsx
+++ client/src/pages/Quiz.jsx
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
+import LargestNumber from "../components/Questions/LargestNumber";
 import FillInTheBlanks from "../components/Questions/FillInTheBlanks";
+import MatchTheFollowing from "../components/Questions/MatchTheFollowing";
 
-export default function Quiz() {
+export default function Quiz({}) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  let questionsArray = [
+    { type: "choose_largest", options: [1, 2, 3, 4], correct: 3 },
+    {
+      type: "fitb",
+      questions: ["10+10 = ", "10+10 = ", "10+10 = ", "10+10 = "],
+      correct: [20, 20, 20, 20],
+    },
+    { type: "choose_largest", options: [1, 2, 3, 4], correct: 3 },
+    { type: "choose_largest", options: [1, 2, 3, 4], correct: 3 },
+    {
+      type: "fitb",
+      questions: ["10+10 = ", "10+10 = ", "10+10 = ", "10+10 = "],
+      correct: [20, 20, 20, 20],
+    },
+    { type: "choose_largest", options: [1, 2, 3, 4], correct: 3 },
+    {
+      type: "mtf",
+      column1: ["10+10 = ", "20+10 = ", "30+10 = ", "40+10 = "],
+      column2: ["20", "30", "40", "50"],
+      correct: [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+        [3, 3],
+      ],
+    },
+  ];
+
+  const handleSelect = (index) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <>
-      <Sidebar qNo={10} />
-      <FillInTheBlanks
-        queNo={1}
-        questionDetails={{
-          questions: ["1+1 = ", "1+1 = ", "1+1 = ", "1+1 = "],
-        }}
-      />
+      <Sidebar qNo={questionsArray.length} onSelect={handleSelect} />
+      {questionsArray.map((question, index) => {
+        if (index !== selectedIndex) return null;
+
+        switch (question.type) {
+          case "choose_largest":
+            return (
+              <LargestNumber
+                key={index}
+                queNo={index + 1}
+                questionDetails={question}
+              />
+            );
+          case "fitb":
+            return (
+              <FillInTheBlanks
+                key={index}
+                queNo={index + 1}
+                questionDetails={question}
+              />
+            );
+          case "mtf":
+            return (
+              <MatchTheFollowing
+                key={index}
+                queNo={index + 1}
+                questionDetails={question}
+              />
+            );
+          default:
+            return null;
+        }
+      })}
     </>
   );
 }
